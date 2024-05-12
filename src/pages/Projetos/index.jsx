17dfda81779b24dee07260209f7b7f870c0062a4
Arthur Souza <111614529+arthursouza20@users.noreply.github.com>
2024-05-11 21:41:20 -0300

    up

--- src/pages/Projetos/index.jsx
+++ src/pages/Projetos/index.jsx
@@ -5,10 +5,65 @@ function Projetos() {
         <>
             <Header />
             <h1>Projetos</h1>
-            <div class="form-check form-switch">
+            {/* <div class="form-check form-switch">
                 <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
                 <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
+            </div> */}
+
+<main>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>Projetos</h1>
+                </div>
+                <div>
+                    <p>Neste menu fica listado todos os projetos que a Inaper pratica. Para você verificar cada projeto, é só clicar no projeto que deseja entrar.</p>
+                </div>
+                <div class="col">
+                    <p>Para adicionar um novo projeto, clique no botão abaixo.</p>
+                    <button type="button" class="btn btn-primary">Adicionar Projeto</button>
+                </div>
             </div>
+        </div>
+        <br/>
+        <br/>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title">Doação</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Saber mais</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title">Oficinas</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
         </>
 
     );
